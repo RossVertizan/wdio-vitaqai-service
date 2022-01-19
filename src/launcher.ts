a0d0da21670c2b3aa5f3416b60caed67314f03ed
Ross Addinall <ross@vertizan.com>
2022-01-19 15:20:45 +0000

    Added checks for required fields on the service options and that the framework is set correctly All checks get done before an error is thrown

--- src/launcher.ts
+++ src/launcher.ts
@@ -38,10 +38,13 @@ export default class VitaqLauncher {
     async onPrepare (config: VtqTestRunner, capabilities: unknown) {
         log.debug("Running the vitaq-service onPrepare method")
         log.debug("config.specs: ", config.specs)
+        let everythingOK: boolean = true
+
+        // ==== Check the specs in the config ====
         // Check to see if the specs have been grouped
         // Expect a single group, so specs with a length of 1
         // and the single spec entry is itself an array with 1 or more entries
-        let specsLookGood = true;
+        let specsLookGood: boolean = true;
         if (typeof config.specs !== "undefined") {
             if (config.specs.length === 1) {
                 if (Array.isArray(config.specs[0])) {
@@ -76,6 +79,47 @@ export default class VitaqLauncher {
             log.error("The specs section should look something like: 'specs: [ ['./test/actions/*.js'] ]");
             log.error("For more information see: https://vitaq.online/documentation/gettingStarted#setting-up-webdriverio");
             throw new SevereServiceError('Specs are not grouped');
+        }
+
+        // ==== Check the options for required fields ====
+        let optionsLookGood: boolean = true;
+        if (typeof this._options.userName === "undefined") {
+            log.error("userName is not defined in the vitaqai options");
+            log.error("userName is the e-mail address used for your VitaqAI account");
+            optionsLookGood = false
+        }
+        if (typeof this._options.projectName === "undefined") {
+            log.error("projectName is not defined in the vitaqai options - t");
+            log.error("projectName is the name of the project which contains the test activity");
+            optionsLookGood = false
+        }
+        if (typeof this._options.testActivityName === "undefined") {
+            log.error("testActivityName is not defined in the vitaqai options");
+            log.error("testActivityName is the name of the test activity you wish to test")
+            optionsLookGood = false
+        }
+        if (typeof this._options.userAPIKey === "undefined") {
+            log.error("userAPIKey is not defined in the vitaqai options");
+            log.error("You can get this by logging into your account at https://vitaq.online and in the top right corner choosing <user>->Get API Key")
+            optionsLookGood = false
+        }
+        if (!optionsLookGood) {
+            log.error("Vitaq requires that you specify some options in the services section of the wdio.conf file");
+            log.error("For more information see: https://vitaq.online/documentation/gettingStarted#setting-up-webdriverio");
+            everythingOK = false;
+        }
+
+        // ==== Check the framework in the config ====
+        if (config.framework !== "vitaq-mocha") {
+            log.error("Vitaq uses a modified version of the Mocha testing framework")
+            log.error("You need to install this modified framework and specify it in the framework section of the wdio.conf file")
+            log.error("For more information see: https://vitaq.online/documentation/gettingStarted#setting-up-webdriverio");
+            everythingOK = false;
+        }
+
+        // Throw SevereServiceError is we detected any issues
+        if (! everythingOK) {
+            throw new SevereServiceError('Errors detected in the configuration for running Vitaq');
         }
     }
 
