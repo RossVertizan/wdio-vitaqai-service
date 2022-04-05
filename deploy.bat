c4d4927493ab00c8b9dcf9721e418e98a21ea797
Ross Addinall <ross@vertizan.com>
2022-04-05 17:23:32 +0100

    Changed the package name

--- deploy.bat
+++ deploy.bat
@@ -8,8 +8,8 @@ echo[
 echo Should we increment the patch number?
 SET /P PATCH=Increment patch (YES to increment)? %=%
 if %PATCH% == YES (
-    npm version patch
-    git push
+    CALL npm version patch
+    CALL git push
 )
 
 REM Publish it to the local and private cloud repos
@@ -25,12 +25,12 @@ echo[
 echo Would you like to push this to the private cloud repository (pkgs.vitaq.online) ?
 SET /P PRIVATE_CLOUD=Push to private cloud repository (YES to push)? %=%
 IF %PRIVATE_CLOUD% == YES (
-    npm publish --registry https://pkgs.vitaq.online
+    CALL npm publish --registry https://pkgs.vitaq.online
 )
 
 echo[
 echo Would you like to push this to the public NPM repository (npmjs.org) ?
 SET /P PUBLIC_CLOUD=Push to public cloud repository (YES to push)? %=%
 IF %PUBLIC_CLOUD% == YES (
-    npm publish --registry https://registry.npmjs.org
+    CALL npm publish --registry https://registry.npmjs.org
 )
